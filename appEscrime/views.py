3c4dbc63f68edef4e00a5c2b29bfb50359c7ec80
Doucet Noam <o22203566@info03-24.iut45.univ-orleans.fr>
2023-10-25 17:26:06 +0200

    debut connexion

--- appEscrime/views.py
+++ appEscrime/views.py
@@ -1,5 +1,13 @@
 from .app import app
 from flask import render_template
+from flask_login import login_user , current_user, logout_user
+from flask import request
+from flask_login import login_required
+from wtforms import StringField , HiddenField
+from wtforms.validators import DataRequired
+from wtforms import PasswordField
+
+
 
 @app.route("/")
 def home():
@@ -12,3 +20,22 @@ def informations():
     return render_template(
         "informations.html"
     )
+@app.route("/connexion/")
+def connexion():
+    return render_template(
+        "connexion.html"
+    )
+
+class LoginForm(FlaskForm):
+    username=StringField('Username')
+    password=PasswordField("Password")
+    next=HiddenField()
+    
+    def get_authenticated_user(self):
+        user = User.query.get(self.username.data)
+        if user is None:
+            return None
+        m=sha256()
+        m.update(self.password.data.encode())
+        passwd= m.hexdigest()
+        return user if passwd == user.password else None
