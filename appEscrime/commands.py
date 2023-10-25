b94a2dbaae8b5e0697edc7901834602912da6bb7
Arthur Goudal <arthur.goudal@etu.univ-orleans.fr>
2023-10-25 21:45:43 +0200

    Peuplement partiel de la base de données : informations hors compétition

--- appEscrime/commands.py
+++ appEscrime/commands.py
@@ -1,6 +1,8 @@
 """Module contenant les commandes de l'application."""
+from datetime import datetime
 from hashlib import sha256
 import click
+import csv
 import os
 from .app import app , db
 from .models import *
@@ -13,52 +15,77 @@ def loaddb():
     db.create_all()
 
     # création du type de phase de poule
-    db.session.add(Type_phase(libelle = 'Poule', nb_touches = 5))
+    types_phase = {'Poule': Type_phase(libelle = 'Poule', nb_touches = 5)}
+    for type_phase in types_phase.values():
+        db.session.add(type_phase)
 
     # création des 3 armes possibles
-    db.session.add(Arme(libelle = 'Fleuret'))
-    db.session.add(Arme(libelle = 'Epée'))
-    db.session.add(Arme(libelle = 'Sabre'))
+    armes = {'Fleuret': Arme(libelle = 'Fleuret'),
+             'Epée': Arme(libelle = 'Epée'),
+             'Sabre': Arme(libelle = 'Sabre')}
+    for arme in armes.values():
+        db.session.add(arme)
 
     # création des 9 catégories possibles
-    db.session.add(Categorie(libelle = 'M13', age_maxi = 13))
-    db.session.add(Categorie(libelle = 'M15', age_maxi = 15))
-    db.session.add(Categorie(libelle = 'M17', age_maxi = 17))
-    db.session.add(Categorie(libelle = 'M20', age_maxi = 20))
-    db.session.add(Categorie(libelle = 'Seniors', age_maxi = 39))
-    db.session.add(Categorie(libelle = 'Vétérans1', age_maxi = 49))
-    db.session.add(Categorie(libelle = 'Vétérans2', age_maxi = 59))
-    db.session.add(Categorie(libelle = 'Vétérans3', age_maxi = 69))
-    db.session.add(Categorie(libelle = 'Vétérans4', age_maxi = 1000))
-
-
-
-
-    for filename in os.listdir('data'):
-        # chargement de notre jeu de données
-        with open(filename, 'r', encoding='utf-8') as file:
-            books = yaml.safe_load(file)
-
-        # première passe: création de tous les auteurs
-        authors = {}
-        for b in books:
-            a = b["author"]
-            if a not in authors:
-                o = Author(name=a)
-                db.session.add(o)
-                authors[a] = o
-                db.session.commit ()
-
-        # deuxième passe: création de tous les livres
-        for b in books:
-            a = authors[b["author"]]
-            o = Book(price = b["price"],
-                    title = b["title"],
-                    url = b["url"] ,
-                    img = b["img"] ,
-                    author_id = a.id)
-        db.session.add(o)
+    categories = {'M13': Categorie(libelle = 'M13', age_maxi = 13),
+                  'M15': Categorie(libelle = 'M15', age_maxi = 15),
+                  'M17': Categorie(libelle = 'M17', age_maxi = 17),
+                  'M20': Categorie(libelle = 'M20', age_maxi = 20),
+                  'Seniors': Categorie(libelle = 'Seniors', age_maxi = 39),
+                  'Vétérans1': Categorie(libelle = 'Vétérans1', age_maxi = 49),
+                  'Vétérans2': Categorie(libelle = 'Vétérans2', age_maxi = 59),
+                  'Vétérans3': Categorie(libelle = 'Vétérans3', age_maxi = 69),
+                  'Vétérans4': Categorie(libelle = 'Vétérans4', age_maxi = -1)}
+    for categorie in categories.values():
+        db.session.add(categorie)
+    
     db.session.commit()
+
+    escrimeurs = dict()
+    clubs = dict()
+
+    for nom_fichier in os.listdir('data'):
+        # chargement de notre jeu de données
+        with open(nom_fichier, newline = '', encoding = 'utf-8') as fichier:
+            lecteur = csv.DictReader(fichier)
+            contenu = nom_fichier.split('_')
+            if '-' in contenu[3]:
+                split_cat = contenu[3].split('-')
+                contenu[3] = split_cat[1][:-1] + split_cat[-1]
+            
+            if contenu[0] == 'classement':
+                
+                for ligne in lecteur:
+                    
+                    nom_club = ligne['club']
+                    if nom_club not in clubs:
+                        club = Club(nom = nom_club)
+                        clubs[nom_club] = club
+                        db.session.add(club)
+                    
+                    licence = ligne['adherent']
+                    if licence not in escrimeurs:
+                        naissance = ligne['date naissance'].split('/')
+                        club = clubs[ligne['club']]
+                        escrimeur = Escrimeur(num_licence = licence,
+                                              prenom = ligne['prenom'],
+                                              nom = ligne['nom'],
+                                              sexe = contenu[2],
+                                              date_naissance = datetime(int(naissance[2]), int(naissance[1]), int(naissance[0])),
+                                              club_id = club.id)
+                        escrimeurs[licence] = escrimeur
+                        db.session.add(escrimeur)
+
+                    arme = armes[contenu[1]]
+                    categorie = categories[contenu[3]]
+                    escrimeur = escrimeurs[ligne['adherent']]
+                    db.session.add(Categorisation(classement = ligne['rang'],
+                                                  points = ligne['points'],
+                                                  num_licence = escrimeur.num_licence,
+                                                  id_arme = arme.id,
+                                                  id_categorie = categorie.id))
+                    
+                    db.session.commit()
 
 @app.cli.command()
 def syncdb():
